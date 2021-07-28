db62f4cfaea0068b63024016691257ebba0c5987
xBlazeTECH <xBlazeTECH@users.noreply.github.com>
2021-07-28 16:29:04 -0400

    Code Cleanup

--- esxsupport/sv_esxsupport.lua
+++ esxsupport/sv_esxsupport.lua
@@ -165,81 +165,85 @@ if pluginConfig.enabled then
     -- EVENT_RECORD_ADDED
     RegisterServerEvent('SonoranCAD::pushevents:RecordAdded')
     AddEventHandler('SonoranCAD::pushevents:RecordAdded', function(record)
+        -- Check to see if we should be issuing fines.
         if not pluginConfig.issueFines then return end
-        debugLog("Processing new record")
-        debugLog(json.encode(record))
-        local index = 1
-        local citation = {
-            issuer = nil,
-            first = nil,
-            last = nil,
-            fine = 0
-        } -- issuer, first, last, fine
+        debugLog("Receieved new record")
+
         local isFineable = false
         for _, formName in pairs(pluginConfig.fineableForms) do
             if record.name:upper() == formName:upper() then isFineable = true end
         end
         if isFineable then
+            -- Create empty citation object
+            local citation = {
+                issuer = nil,   -- Issuer of the fine
+                first = nil,    -- First name of the fine target
+                last = nil,     -- Last name of the fine target
+                fine = 0        -- Total sum of all fineable offenses
+            }
+            debugLog(record.name:upper() .. " is a fineable record.")
+            -- Iterate the sections of the record
             for k, sec in pairs(record.sections) do
+                -- Iterate the fields of the record section
                 for _, field in pairs(sec.fields) do
-                    debugLog(index .. json.encode(field))
-                    index = index+1
-                    if field.data.fine then
-                        citation.fine = citation.fine + tonumber(field.data.fine)
-                        debugLog("Added Speeding Fine of:" .. field.data.fine)
-                    else
-                        if field.uid == 'first' then
-                            citation.first = field.value
-                            debugLog("First Name =" .. citation.first)
-                        end
-                        if field.uid == 'last' then
-                            citation.last = field.value
-                            debugLog("Last Name =" .. citation.last)
-                        end
-                        if field.label == 'New Field Name' then
-                            if field.data then
-                                if field.data.charges then
-                                    for _, charge in pairs(field.data.charges) do
-                                        debugLog('Added additional charge of $' .. charge.arrestBondAmount .. ' for ' .. charge.arrestCharge)
-                                        citation.fine = citation.fine + tonumber(charge.arrestBondAmount)
-                                    end
-                                end
-                                if field.data.officer then
-                                    citation.issuer = field.data.officer
-                                    debugLog("Fine Issuer = " .. citation.issuer)
+                    -- Store the first name of the fine target
+                    if field.uid == 'first' then citation.first = field.value end
+                    -- Store the last name of the fine target
+                    if field.uid == 'last' then citation.last = field.value end
+                    -- Get "Special" fields from the report
+                    if field.label == 'New Field Name' then
+                        if field.data then
+                            -- Get and store the name of the issuing officer to the citation
+                            if field.data.officer then citation.issuer = field.data.officer end
+                            -- Get and add speeding charges to the citation
+                            if field.data.fine then
+                                citation.fine = citation.fine + tonumber(field.data.fine)
+                                debugLog("Added fine of $" .. field.data.fine .. ' for ' .. field.data.vehicleSpeed .. ' in a ' .. field.data.speedLimit .. 'zone.')
+                            end
+                            -- Get and add other charges to the citation
+                            if field.data.charges then
+                                for _, charge in pairs(field.data.charges) do
+                                    citation.fine = citation.fine + tonumber(charge.arrestBondAmount)
+                                    debugLog('Added fine of $' .. charge.arrestBondAmount .. ' for ' .. charge.arrestCharge)
                                 end
                             end
                         end
                     end
                 end
             end
-            if not citation.first or not citation.last or not citation.fine then return end
 
+            debugLog("New Citation to Issue:")
+            debugLog("Issuer: " .. citation.issuer)
+            debugLog("Issued To: " .. citation.first .. " " .. citation.last)
+            debugLog("Total Fines: $" .. citation.fine)
+
+            -- If the citation is missing a first name or a last name we can't issue the fine.
+            if citation.first == '' or citation.last == '' then return end
+
+            -- Find the civilian that matches the citation and issue them a fine.
             local xPlayers = ESX.GetPlayers()
-
             for i=1, #xPlayers, 1 do
                 local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
                 if xPlayer.getName() == citation.first .. ' ' .. citation.last then
+                    -- Issue the fine to the player, either by billing or direct withdrawl
                     if pluginConfig.useBilling then
-                        xPlayer.triggerEvent('SonoranCAD::esxsupport:issueFine', xPlayer, citation.fine)
+                        errorLog("Billing system is not yet implemented. Please disable it in the config.")
+                        --xPlayer.triggerEvent('SonoranCAD::esxsupport:issueFine', xPlayer, citation.fine)
                     else
                         xPlayer.removeAccountMoney('bank', citation.fine)
-                    end
-                    if pluginConfig.fineNotify then
                         ESX.SavePlayer(xPlayer)
-                        if citation.issuer ~= nil then
-                            TriggerClientEvent('chat:addMessage', -1, {
-                                color = { 255, 0, 0},
-                                multiline = true,
-                                args = { xPlayer.getName() .. ' has been issued a fine of $' .. citation.fine .. ' by ' .. citation.issuer }
-                            })
-                        else
-                            TriggerClientEvent('chat:addMessage', -1, {
-                                color = { 255, 0, 0},
-                                multiline = true,
-                                args = { xPlayer.getName() .. ' has been issued a fine of $' .. citation.fine }
-                            })
-                        end
+                    end
+                    -- Send a notification message to the server that the fine has been issued and who issued the fine.
+                    if pluginConfig.fineNotify then
+                        -- Set the message to be displayed to the users.
+                        local finemessage = xPlayer.getName() .. ' has been issued a fine of $' .. citation.fine
+                        -- Add issuers name if present
+                        if citation.issuer ~= '' then finemessage = finemessage .. ' by ' .. citation.issuer end
+                        TriggerClientEvent('chat:addMessage', -1, {
+                            color = { 255, 0, 0 },
+                            multiline = true,
+                            args = { finemessage }
+                        })
                     end
                 end
             end
