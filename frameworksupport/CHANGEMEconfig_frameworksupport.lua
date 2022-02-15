053f5142e7f9fbc0a489be502304e1dc8f3d9fa5
ADRNALN <32118783+Tiimon@users.noreply.github.com>
2022-02-15 10:32:50 -0800

    Correct Config

--- frameworksupport/CHANGEMEconfig_frameworksupport.lua
+++ frameworksupport/CHANGEMEconfig_frameworksupport.lua
@@ -1,33 +1,29 @@
 --[[
     Sonoran Plugins
 
-    ESXSupport Plugin Configuration
+    frameworksupport Plugin Configuration
 
     Put all needed configuration in this file.
 
 ]]
 local config = {
-    enabled = false,
+    enabled = true,
     configVersion = "4.0",
-    pluginName = "esxsupport", -- name your plugin here
+    pluginName = "frameworksupport", -- name your plugin here
     pluginAuthor = "SonoranCAD", -- author
     requiresPlugins = {}, -- required plugins for this plugin to work, separated by commas
 
     -- Newer ESX versions use license instead of steam for identity, specify the other below if different
     identityType = "license",
     -- Some ESX versions don't use the prefix (such as license:abcdef), set to false to disable the prefix
-    usePrefix = false,
+    usePrefix = true,
     -- Use QBUS (a renamed ESX with some changes)? Set this to "true"
-    usingQbus = false,
-    -- Have a renamed QBUS Framework? (like RepentzFW) change the "QBCore" name here to whatever the event name uses
-    QbusEventName = "QBCore",
-    -- If using qbus, enter the name of the core resource (such as qb-core)
-    QbusResourceName = "qb-core",
+    usingQBCore = true,
 
     -- Fine payment system
-    issueFines = false,                 -- Use the fine system
+    issueFines = true,                 -- Use the fine system
     fineNotify = false,                 -- Send a message in chat when someone is fined.
-    fineableForms = {"Arrest Report"},  -- List of form names that should issue fines (Don't Include Warrants or Bolos)
+    fineableForms = {"Arrest Report", "General Citation"},  -- List of form names that should issue fines (Don't Include Warrants or Bolos)
 
     -- ESX Legacy Support (Created for and tested using ESX v1.1.0 esx_identity v1.0.2)
     legacyESX = false                   -- Set to true if default settings do not get character name properly (older esx_identity/ESX legacy versions)
