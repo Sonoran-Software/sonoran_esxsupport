256d7bdb42989e3476893072a86751a1c4bf29e8
ADRNALN <32118783+Tiimon@users.noreply.github.com>
2022-02-15 10:32:23 -0800

    QBCore Compatibility
    ESX & QBCore are the new mainstream frameworks being used by servers. This adds direct compatibility with QBCore.

--- frameworksupport/cl_frameworksupport.lua
+++ frameworksupport/cl_frameworksupport.lua
@@ -1,41 +1,35 @@
 --[[
     Sonaran CAD Plugins
 
-    Plugin Name: esxsupport
+    Plugin Name: frameworksupport
     Creator: Sonoran Software Systems LLC
-    Description: Enable using ESX (or ESX clones) character information in Sonoran integration plugins
+    Description: Enable using ESX or QBCore character information in Sonoran integration plugins
 ]]
 
-local pluginConfig = Config.GetPluginConfig("esxsupport")
+local pluginConfig = Config.GetPluginConfig("frameworksupport")
 
 if pluginConfig.enabled then
-
-    Citizen.CreateThread(function()
-        ---------------------------------------------------------------------------
-        -- ESX Integration Initialization/Events/Functions
-        ---------------------------------------------------------------------------
-        -- Initialize ESX Framework hooks to allow obtaining data
+    CreateThread(function()
+        local QBCore = exports['qb-core']:GetCoreObject()
         PlayerData = {}
         ESX = nil
 
-        Citizen.CreateThread(function()
+        CreateThread(function()
             while ESX == nil do
-                if pluginConfig.usingQbus then
-                    TriggerEvent(pluginConfig.QbusEventName .. ':GetObject', function(obj) ESX = obj end)
-                else
+                if not pluginConfig.usingQBCore then
                     TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
                 end
-                Citizen.Wait(10)
+                Wait(10)
             end
 
-            if pluginConfig.usingQbus then
-                while ESX.Functions.GetPlayerData() == nil do
-                    Citizen.Wait(10)
+            if pluginConfig.usingQBCore then
+                while QBCore.Functions.GetPlayerData() == nil do
+                    Wait(10)
                 end
-                PlayerData = ESX.Functions.GetPlayerData()
+                PlayerData = QBCore.Functions.GetPlayerData()
             else
                 while ESX.GetPlayerData() == nil do
-                    Citizen.Wait(10)
+                    Wait(10)
                 end
                 PlayerData = ESX.GetPlayerData()
             end
@@ -44,35 +38,35 @@ if pluginConfig.enabled then
         -- Listen for when new players load into the game
         RegisterNetEvent('esx:playerLoaded')
         AddEventHandler('esx:playerLoaded', function(xPlayer)
-            if pluginConfig.usingQbus then
-                PlayerData = ESX.Functions.GetPlayerData()
+            if pluginConfig.usingQBCore then
+                PlayerData = QBCore.Functions.GetPlayerData()
             else
                 PlayerData = xPlayer
             end
         end)
         -- Listen for when jobs are changed in esx_jobs
-        if pluginConfig.usingQbus then
-            RegisterNetEvent(pluginConfig.QbusEventName .. ':Client:OnJobUpdate')
-            AddEventHandler(pluginConfig.QbusEventName .. ':Client:OnJobUpdate', function(job)
+        if pluginConfig.usingQBCore then
+            RegisterNetEvent('QBCore:Client:OnJobUpdate')
+            AddEventHandler('QBCore:Client:OnJobUpdate', function(job)
                 PlayerData.job = job
-                if PlayerData.job.onduty == true then -- QBUS job.onduty is false when on duty??? okayyyyy
+                if PlayerData.job.onduty == true then
                     PlayerData.job.name = 'offduty' .. PlayerData.job.name
                 end
-                TriggerServerEvent('SonoranCAD::esxsupport:refreshJobCache')
-                TriggerEvent('SonoranCAD::esxsupport:JobUpdate', job)
+                TriggerServerEvent('SonoranCAD::frameworksupport:refreshJobCache')
+                TriggerEvent('SonoranCAD::frameworksupport:JobUpdate', job)
             end)
         else
             RegisterNetEvent('esx:setJob')
             AddEventHandler('esx:setJob', function(job)
                 PlayerData.job = job
-                TriggerServerEvent('SonoranCAD::esxsupport:refreshJobCache')
-                TriggerEvent('SonoranCAD::esxsupport:JobUpdate', job)
+                TriggerServerEvent('SonoranCAD::frameworksupport:refreshJobCache')
+                TriggerEvent('SonoranCAD::frameworksupport:JobUpdate', job)
             end)
         end
         -- QBUS onduty change (ESX typically uses jobs to change duty instead)
-        if pluginConfig.usingQbus then
-            RegisterNetEvent(pluginConfig.QbusEventName .. ':Client:SetDuty')
-            AddEventHandler(pluginConfig.QbusEventName .. ':Client:SetDuty', function(onduty)
+        if pluginConfig.usingQBCore then
+            RegisterNetEvent('QBCore:Client:SetDuty')
+            AddEventHandler('QBCore:Client:SetDuty', function(onduty)
                 local job = PlayerData.job
                 if onduty then
                     job.name = string.gsub(job.name,'offduty','')
@@ -80,8 +74,8 @@ if pluginConfig.enabled then
                     job.name = 'offduty' .. job.name
                 end
                 PlayerData.job = job
-                TriggerServerEvent('SonoranCAD::esxsupport:refreshJobCache')
-                TriggerEvent('SonoranCAD::esxsupport:JobUpdate', job)
+                TriggerServerEvent('SonoranCAD::frameworksupport:refreshJobCache')
+                TriggerEvent('SonoranCAD::frameworksupport:JobUpdate', job)
             end)
         end
 
@@ -89,8 +83,8 @@ if pluginConfig.enabled then
         -- This event listens for data from the server when requested
         local recievedIdentity = false
         returnedIdentity = nil
-        RegisterNetEvent('SonoranCAD::esxsupport:returnIdentity')
-        AddEventHandler('SonoranCAD::esxsupport:returnIdentity', function(data)
+        RegisterNetEvent('SonoranCAD::frameworksupport:returnIdentity')
+        AddEventHandler('SonoranCAD::frameworksupport:returnIdentity', function(data)
             recievedIdentity = true
             if data.job == nil then
                 warnLog("Warning: no identity data was found.")
@@ -102,15 +96,13 @@ if pluginConfig.enabled then
         function GetIdentity(callback)
             recievedIdentity = false
             returnIdentity = false
-            TriggerServerEvent("SonoranCAD::esxsupport:getIdentity")
+            TriggerServerEvent("SonoranCAD::frameworksupport:getIdentity")
             local timeStamp = GetGameTimer()
             while not recievedIdentity do
-                Citizen.Wait(0)
+                Wait(0)
             end
             callback(returnedIdentity)
         end
         
     end)
-    
-
 end
